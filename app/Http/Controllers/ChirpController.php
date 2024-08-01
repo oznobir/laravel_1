745717ffa988c2be21f4bd06bbd57035b210df59
Oznobishin_Ruslan <oznobir@gmail.com>
2024-08-01 15:35:42 +0300

    +first - chirp new

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Factory|View|Application
+     */
+    public function index(): Factory|View|Application
+    {
+        return view('chirps.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+        $request->user()->chirps()->create($validated);
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
